80d4d464ce77455eb1e656879b1d752a02d6c25a
Trần Mạnh Hùng <hungtm@ows.com.vn>
2019-07-19 22:01:55 +0700

    add effect typerwriter

--- public/js/index.js
+++ public/js/index.js
@@ -1,9 +1,9 @@
 // Các bạn có thể thay đổi giá trị các biến môi trường ở đây:
-var radius = 240; // Độ rộng vòng xoay
+var radius = 300; // Độ rộng vòng xoay
 var autoRotate = true; // Tự động xoay hay không
 var rotateSpeed = -60; // đơn vị: giây/vòng. thời gian để xoay hết 1 vòng, dấu trừ để xoay ngược lại
-var imgWidth = 120; // độ rộng ảnh (tính theo px)
-var imgHeight = 170; // độ cao ảnh (tính theo px)
+var imgWidth = 150; // độ rộng ảnh (tính theo px)
+var imgHeight = 200; // độ cao ảnh (tính theo px)
 
 // Link nhạc nền - cho bằng null nếu không muốn nhạc nền
 var bgMusicURL = false;
@@ -95,6 +95,8 @@ $(document).ready(function(){
 $('.bird').click(function(){
   $('.typewrite').css('display','block')
   $('#drag-container').hide();
+  $('.danhchu').hide();
+
   $('.header').hide();
   var content = $('.typewrite').html();
   //find the length of the content
@@ -117,7 +119,62 @@ $('.bird').click(function(){
  
 });
 
+var TxtType = function(el, toRotate, period) {
+  this.toRotate = toRotate;
+  this.el = el;
+  this.loopNum = 0;
+  this.period = parseInt(period, 10) || 2000;
+  this.txt = '';
+  this.tick();
+  this.isDeleting = false;
+};
 
+TxtType.prototype.tick = function() {
+  var i = this.loopNum % this.toRotate.length;
+  var fullTxt = this.toRotate[i];
+
+  if (this.isDeleting) {
+  this.txt = fullTxt.substring(0, this.txt.length - 1);
+  } else {
+  this.txt = fullTxt.substring(0, this.txt.length + 1);
+  }
+
+  this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+  var that = this;
+  var delta = 200 - Math.random() * 100;
+
+  if (this.isDeleting) { delta /= 2; }
+
+  if (!this.isDeleting && this.txt === fullTxt) {
+  delta = this.period;
+  this.isDeleting = true;
+  } else if (this.isDeleting && this.txt === '') {
+  this.isDeleting = false;
+  this.loopNum++;
+  delta = 500;
+  }
+
+  setTimeout(function() {
+  that.tick();
+  }, delta);
+};
+
+window.onload = function() {
+  var elements = document.getElementsByClassName('typewrited');
+  for (var i=0; i<elements.length; i++) {
+      var toRotate = elements[i].getAttribute('data-type');
+      var period = elements[i].getAttribute('data-period');
+      if (toRotate) {
+        new TxtType(elements[i], JSON.parse(toRotate), period);
+      }
+  }
+  // INJECT CSS
+  var css = document.createElement("style");
+  css.type = "text/css";
+  css.innerHTML = ".typewrited > .wrap {color:white}";
+  document.body.appendChild(css);
+};
 
 setTimeout(init, 100);
 
